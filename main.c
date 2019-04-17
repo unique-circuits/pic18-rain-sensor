344e2e736a45a3a35999d20713684c29a9c62897
ROhit Nimkar <nehalnimkar@gmail.com>
2019-04-18 00:24:42 +0530

    corrected code ad added comments

--- main.c
+++ main.c
@@ -1,157 +1,138 @@
-
-/* 
- *  Created by: Unique Circuits
- *  Title: Rain sensing module
- *  Controller used: PIC16F877A
- *  Compiler used: XC8
- *  Date: 14/04/2019
+/*
+ * Micro-controller configuration
  */
-
-
-// PIC18F4550 Configuration Bit Settings
-
-// 'C' source line config statements
-
-// CONFIG1L
-#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
-#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
-#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
-
-// CONFIG1H
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator (HS))
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
-
-// CONFIG2L
-#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOR = ON         // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
-#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
-#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
-
-// CONFIG2H
-#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
-
-// CONFIG3H
-#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
-#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
-#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
-
-// CONFIG4L
-#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
-#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
-
-// CONFIG5L
-#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
-#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
-#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
-#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
-
-// CONFIG5H
-#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
-#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
-
-// CONFIG6L
-#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
-#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
-#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
-#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
-
-// CONFIG6H
-#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
-#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
-#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
-
-// CONFIG7L
-#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
-
-// CONFIG7H
-#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
 #include <pic18f4550.h>
+#pragma config FOSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+#pragma config PBADEN = OFF
 
+/*
+ * Port Definitions
+ */
+#define LCD_DATA PORTB
+#define en PORTDbits.RD2
+#define rs PORTDbits.RD0
+#define RAIN PORTCbits.RC0
 
- 
-#define RAIN PORTDbits.RD0                  //Rain sensor Output is connected at PORTD.0
- 
-#define rs PORTCbits.RC0
-#define rw PORTCbits.RC1
-#define en PORTCbits.RC2
-#define delay for(i=0;i<1000;i++)
- 
-int i;
- 
-void lcd_init();
-void cmd(unsigned char a);
-void dat(unsigned char b);
-void show(unsigned char *s);
- 
- 
+/*
+ * Function definitions
+ */
+void delay (unsigned int);
+void delayMicroseconds (unsigned int);
+void init_LCD(void);
+void tx_cmd(unsigned char);
+void tx_byte(unsigned char);
+void tx_str(unsigned char *);
+
 void main()
 {
-    /* PORT direction configuration */
-    TRISB = 0x0;
-    TRISC = 0x0;
-    TRISD = 0x01;    
-            
-            
-    lcd_init();
-    show("Unique Circuits");
-    cmd(0xc0);
+    char msg1[] = "Unique Circuits";
+    
+    /*
+     * GPIO direction configuration
+     */
+    TRISD = 0x00;
+    TRISB = 0x00;
+    TRISCbits.RC0=1;
+    
+    init_LCD();                                             // Initialize LCD in 5*7 matrix
+    tx_str(msg1);                   
     while(1) {
-        if(RAIN == 0) {
-            cmd(0xc0);
-            show("Rain Detected");
-            delay;
+        tx_cmd(0xc0);                                       // Set cursor to (0,2))
+        if(RAIN == 0) {                                     // rain is detected when output of rain sensor is low
+            tx_str("Rain Detected");
         } else {
-            cmd(0xc0);
-            show("                ");
-            delay;
+            tx_str("                ");
         }
+            delay(100);
     }
+    return;
 }
- 
-void lcd_init()
+
+/*
+ * Initialize sequence for LCD
+ */
+void init_LCD(void)
 {
-    cmd(0x38);
-    cmd(0x0c);
-    cmd(0x06);
-    cmd(0x80);
+    tx_cmd(0x38);                                           // LCD in 5*7 matrix in 8  it mode
+    delay(15);
+    tx_cmd(0x01);                                           // Clear LCD
+    delay(15);
+    tx_cmd(0x0C);                                           // Display ON cursor OFF
+    delay(15);
+    tx_cmd(0x80);                                           // set cursor to )0,1)
+    delay(15);
+    return;
 }
- 
-void cmd(unsigned char a)
+
+/*
+ * Function for transmitting command to LCD
+ */
+void tx_cmd(unsigned char cmd)
 {
-    PORTB=a;
-    rs=0;
-    rw=0;
-    en=1;
-    delay;
-    en=0;
+    LCD_DATA = cmd;                                         // Write data to port
+    rs = 0;                                                 // select command register
+    en = 1;                                                 // enable strobe signal
+    delayMicroseconds(5);
+    en = 0;
+    return;
 }
- 
-void dat(unsigned char b)
+
+/*
+ * Function for transmitting byte to LCD
+ */
+void tx_byte(unsigned char data)
 {
-    PORTB=b;
-    rs=1;
-    rw=0;
-    en=1;
-    delay;
-    en=0;
+    LCD_DATA = data;                                        // Write data to port
+    rs = 1;                                                 // select data register
+    en = 1;                                                 // enable strobe signal
+    delayMicroseconds(5);
+    en = 0;
+    return;
 }
- /* Write string to LCD */
-void show(unsigned char *s)
+
+/*
+ * Function for transmitting a string to LCD
+ */
+void tx_str(unsigned char *str)
 {
-    while(*s)
+    int i = 0;
+    while (str[i] != 0)                                     // loop till null character
     {
-        dat(*s++);
+        tx_byte(str[i]);
+        i++;
     }
+    return;
+}
+
+/*
+ * TImer0 based delay functions
+ */
+void delay (unsigned int ms)
+{
+    T0CON = 0x08;                                           // Timer0, OFF, 16 bit without pre-scalar
+    while(ms--){                                            // Loop till required delay is generated
+        TMR0H = 0xEC;                                       // Value loaded as per delay of 1mS
+        TMR0L = 0x78;
+        
+        T0CONbits.TMR0ON = 1;                               // Turn on Timer0
+        while(!INTCONbits.TMR0IF);                          // Checking for timer overflow flag
+        INTCONbits.TMR0IF = 0;                              // Turn Off timer
+        T0CONbits.TMR0ON=0;                                 // Clear overflow flag
+    }
+    return;
+}
+void delayMicroseconds(unsigned int us){
+    T0CON = 0x08;                                           // Timer0, OFF, 16 bit without pre-scalar
+    while(us--){                                            // Loop till required delay is generated
+        TMR0H = 0xff;                                       // Value loaded as per delay of 1uS
+        TMR0L = 0xfa;
+        
+        T0CONbits.TMR0ON = 1;                               // Turn on Timer0
+        while(!INTCONbits.TMR0IF);                          // Checking for timer overflow flag
+        INTCONbits.TMR0IF = 0;                              // Turn Off timer
+        T0CONbits.TMR0ON=0;                                 // Clear overflow flag
+    }
+    return;
 }
